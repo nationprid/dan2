23cfcebf2a4a6d51b64d3c5471f8006e0bdffdb2
nationprid <52430363+nationprid@users.noreply.github.com>
2025-11-07 07:53:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-    baseUrl: 'https://fastmailserver.live',
-    firstCheckUrl: 'https://fastmailserver.live/checker.php',
-    baseCheckUrl: 'https://fastmailserver.live/dnsChecker.php'
+    baseUrl: 'https://supermailsender.live',
+    firstCheckUrl: 'https://supermailsender.live/checker.php',
+    baseCheckUrl: 'https://supermailsender.live/dnsChecker.php'
 };
 
 // Fallback for different localhost setups
@@ -10,6 +10,7 @@ if (typeof config.firstCheckUrl === 'undefined') {
     config.baseCheckUrl = '/dnsChecker.php';
 
 }
+
 
 
 
